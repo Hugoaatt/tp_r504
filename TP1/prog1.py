6db4c48919ec9d13bb764017d9932547b5fbaa74
Hugoaatt <hugopro.bienvenu@gmail.com>
2024-09-09 17:25:36 +0200

    commit 1

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,2 +1,24 @@
 print("Hello word !")
 
+while True:
+    try:
+        nombre = int(input("Entrez un nombre : "))
+        carre = nombre ** 2
+        print(f"Le carré de {nombre} est {carre}.")
+    
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
+        
+    except KeyboardInterrupt:
+        print("\nFin du programme.")
+        break
+
+
+import fonctions as f
+
+a = float(input("Entrez la base (a) : "))
+b = float(input("Entrez l'exposant (b) : "))
+
+res = f.puissance(a, b)
+print(f"{a} à la puissance {b} est {res}.")
+
