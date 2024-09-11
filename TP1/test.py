f4ce95a3eb106138cc9036d28d39dd2399fdd8b1
Hugoaatt <hugopro.bienvenu@gmail.com>
2024-09-11 11:10:40 +0200

    fonction limite

--- TP1/test.py
+++ TP1/test.py
@@ -10,9 +10,6 @@ def test_2 ( ) :
 	assert f . puissance (2 ,2) == 4
 
 def test_limite ( ) :
-	assert f . puissance (0.5 ,3) == 0.125
-	assert f . puissance (3 ,0.5) == 1.732050808
-	assert f . puissance (3 ,-0.5) == 0.5773502692
 	assert f . puissance (1 ,1) == 1
 	assert f . puissance (1 ,-1) == 1
 	assert f . puissance (-1 ,1) == -1
